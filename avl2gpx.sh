31af685caa7e36fabd6ef537f756dd9e1b239c1d
Bassem <bassem@bassem-laptop.(none)>
2009-11-24 07:49:10 +0200

    first upload

---
+++ avl2gpx.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+FILE="$1"
+OUT="$FILE.gpx"
+
+rm $OUT
+
+# write xml header
+echo "<?xml version='1.0' encoding='UTF-8'?>"  >> $OUT
+echo "<gpx version='1.1' creator='GPSMID' xmlns='http://www.topografix.com/GPX/1/1'>" >> $OUT
+echo "<trk>"  >> $OUT
+echo "<trkseg>" >> $OUT
+
+# proccess the data
+cat $FILE | while read line ; do 
+
+lat=`echo $line | awk -F, '{ print $1 }'`
+lon=`echo $line | awk -F, '{ print $2 }'`
+timestamp=`echo $line | awk -F, '{ print $5 }'`
+
+date=`date -d @$timestamp +%FT%TZ`
+
+# write the data
+echo  "<trkpt lat='$lat' lon='$lon' >"  >> $OUT
+echo  "<time>$date</time>"  >> $OUT
+echo "</trkpt>"  >> $OUT
+
+done
+
+# write xml footer
+echo  "</trkseg>"  >> $OUT
+echo  "</trk>"  >> $OUTecho  "</gpx>"  >> $OUT
+
+exit 0
+
